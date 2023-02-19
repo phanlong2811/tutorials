19c6e92ae321bc9a915b116954284362b5bbb04c
Phan Long <ptlong2811@gmail.com>
2023-02-19 22:18:56 +0700

    https://go.dev/doc/tutorial/greetings-multiple-people

--- Go/docs/Tutorial: Create a module/hello/hello.go
+++ Go/docs/Tutorial: Create a module/hello/hello.go
@@ -13,8 +13,12 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
     
-    // Get a greeting message and print it.
-    message, err := greetings.Hello("Phan Long")
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
+    // Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
+
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
@@ -22,5 +26,5 @@ func main() {
     }
     // If no error was returned, print the returned message
     // to the console.
-    fmt.Println(message)
+    fmt.Println(messages)
 }
